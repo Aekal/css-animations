12117a977f844913969ee2651d17e73ce956933a
Damian Fiałkiewicz <aekal4@gmail.com>
2016-11-30 15:33:07 +0100

    Add pop-up

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 var gears = document.getElementById("gears-container");
 var cards = document.getElementById("cards-container");
+var popup = document.getElementById("popup-container");
 
 document.getElementById("btn-gears").addEventListener("click", function() {
 	gears.style.display = "block";
 	cards.style.display = "none";
+	popup.style.display = "none";
 })
 document.getElementById("btn-cards").addEventListener("click", function() {
 	gears.style.display = "none";
 	cards.style.display = "block";
+	popup.style.display = "none";
 })
 document.getElementById("btn-popup").addEventListener("click", function() {
 	gears.style.display = "none";
+	cards.style.display = "none";
+	popup.style.display = "block";
 })
