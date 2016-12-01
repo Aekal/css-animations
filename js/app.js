14fba1d16a08ec2955a7684abb780cc15d244829
Damian Fiałkiewicz <aekal4@gmail.com>
2016-12-01 23:09:24 +0100

    Add dropdown menu w/o animation

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 var gears = document.getElementById("gears-container");
-var cards = document.getElementById("cards-container");
-var popup = document.getElementById("popup-container");
+var wrapper = document.getElementsByClassName("wrapper");
 
 // GEARS
 document.getElementById("btn-gears").addEventListener("click", function() {
-	gears.style.display = "block";
-	cards.style.display = "none";
-	popup.style.display = "none";
+	showContainer("gears-container");
 });
 gears.addEventListener("click", function() {
 	for (i = 0; i < this.children.length; i++) {
@@ -16,15 +13,23 @@ gears.addEventListener("click", function() {
 
 //CARDS
 document.getElementById("btn-cards").addEventListener("click", function() {
-	gears.style.display = "none";
-	cards.style.display = "block";
-	popup.style.display = "none";
+	showContainer("cards-container");
 });
 
 //POPUP
 document.getElementById("btn-popup").addEventListener("click", function() {
-	gears.style.display = "none";
-	cards.style.display = "none";
-	popup.style.display = "block";
-})
-;
+	showContainer("popup-container");
+});
+
+//DROPDOWN MENU
+document.getElementById("btn-menu").addEventListener("click", function() {
+	showContainer("menu-container");
+});
+
+
+function showContainer(id) {
+	for (i = 0; i < wrapper[0].children.length; i++) {
+		wrapper[0].children[i].style.display = "none";
+		document.getElementById(id).style.display = "block";
+	}
+}
