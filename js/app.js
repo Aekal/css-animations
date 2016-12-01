ac00682fb835d2b4b9f616b1ee35b3aa647aca7d
Damian Fiałkiewicz <aekal4@gmail.com>
2016-12-01 20:37:29 +0100

    Add gears pause on click

--- js/app.js
+++ js/app.js
@@ -2,18 +2,29 @@ var gears = document.getElementById("gears-container");
 var cards = document.getElementById("cards-container");
 var popup = document.getElementById("popup-container");
 
+// GEARS
 document.getElementById("btn-gears").addEventListener("click", function() {
 	gears.style.display = "block";
 	cards.style.display = "none";
 	popup.style.display = "none";
-})
+});
+gears.addEventListener("click", function() {
+	for (i = 0; i < this.children.length; i++) {
+		this.children[i].classList.toggle("is-paused");
+	}
+});
+
+//CARDS
 document.getElementById("btn-cards").addEventListener("click", function() {
 	gears.style.display = "none";
 	cards.style.display = "block";
 	popup.style.display = "none";
-})
+});
+
+//POPUP
 document.getElementById("btn-popup").addEventListener("click", function() {
 	gears.style.display = "none";
 	cards.style.display = "none";
 	popup.style.display = "block";
 })
+;
